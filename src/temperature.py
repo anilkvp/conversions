e26a4a0f55fed7d41db6fa92df843632240a82f8
coldstone <anil@myarch.linux>
2024-09-07 09:21:58 +0530

    Initial commit

---
+++ src/temperature.py
@@ -0,0 +1,51 @@
+"""
+
+"""
+
+
+def celsius_to_kelvin(celsius):
+    """
+
+    Args:
+        celsius ():
+
+    Returns:
+
+    """
+    return celsius + 273.15
+
+
+def celsius_to_fahrenheit(celsius):
+    """
+
+    Args:
+        celsius ():
+
+    Returns:
+
+    """
+    return celsius * 1.8 + 32
+
+
+def fahrenheit_to_celsius(fahrenheit):
+    """
+
+    Args:
+        fahrenheit ():
+
+    Returns:
+
+    """
+    return (fahrenheit - 32) / 1.8
+
+
+def kelvin_to_celsius(kelvin):
+    """
+
+    Args:
+        kelvin ():
+
+    Returns:
+
+    """
+    return kelvin - 273.15
